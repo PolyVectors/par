54b28d95f29163fbce47e3153c336e780cc978f6
PolyVectors <PolyVector@protonmail.com>
2025-08-17 01:53:38 +0100

    panic usage into macro

--- par.c
+++ par.c
@@ -1,16 +1,12 @@
 #include "bar.h"
 
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 #ifndef VERSION
 #define VERSION "0.1.0"
 #endif
-
-/* TODO: consolidate into just one print call?
-   TODO: hop on godbolt and check if using fputs is worthwhile */
-#define PANIC_USAGE(...) do { \
-                            fputs("\x1b[1;91merror\x1b[0m: ", stderr); \
-                            fprintf(stderr, __VA_ARGS__); \
-                            usage(1); \
-                        } while (0) \
 
 static void
 usage(int code)
@@ -26,6 +22,17 @@ usage(int code)
 }
 
 static void
+panicusage(const char *fmt, ...)
+{
+    va_list args;
+    va_start (args, fmt);
+
+    fputs("\x1b[1;91merror\x1b[0m: ", stderr);
+    vfprintf(stderr, fmt, args);
+    usage(1);
+}
+
+static void
 run(Config *config)
 {
     Bar *bar = bar_create(config);
@@ -36,7 +43,7 @@ run(Config *config)
         if (XFilterEvent(&ev, bar->window))
             continue;
 
-        /* bit redundant, as event mask only allows for this one event, for now */
+        /* bit redundant as event mask only allows for this one event, for now */
         switch (ev.type) {
         case Expose:
             bar_draw(bar, config);
@@ -57,22 +64,22 @@ main(int argc, char **argv)
 
     for (int i = 1; i < argc; i++) {
         if (config != NULL)
-            PANIC_USAGE("expected no additional arguments after config.\n");
+                panicusage("expected no additional arguments after config.\n");
         else if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help")) {
             if (argc != 2 )
-                PANIC_USAGE("argument `%s` expects no arguments.\n", argv[i]);
+                panicusage("argument `%s` expects no arguments.\n", argv[i]);
             usage(0);
         } else if (!strcmp(argv[i], "-v") || !strcmp(argv[i], "--version")) {
             if (argc != 2 )
-                PANIC_USAGE("argument `%s` expects no arguments.\n", argv[i]);
+                panicusage("argument `%s` expects no arguments.\n", argv[i]);
             printf("par @commit %s\n", VERSION);
         } else if (!strcmp(argv[i], "-c") || !strcmp(argv[i], "--config")) {
             if (i == argc - 1)
-                PANIC_USAGE("expected path after config, got nothing.\n");
+                panicusage("expected path after config, got nothing.\n");
             config = config_new(argv[i + 1]);
             i += 1;
         } else
-            PANIC_USAGE("invalid argument `%s`.\n", argv[i]);
+            panicusage("invalid argument `%s`.\n", argv[i]);
     }
     if (config == NULL)
         return 0;
